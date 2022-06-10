c7d28cb6f8d3369ffd941280daf711a6d02fae93
Sonia Ann Friscia <soniaannfriscia@gmail.com>
2022-06-09 20:02:02 -0400

    Successfully connect db to server and make query returning data in response

--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DB,
+  port: process.env.DB_PORT,
+});
 
 // middleware
 app.use(express.json());
 
 // routes
 app.get('/qa/questions', (req, res) => {
-  res.send('it worked');
-})
+  pool.query(`SELECT *
+  FROM questions q
+  LEFT JOIN answers an
+  ON (q.id = an.question_id)
+  LEFT JOIN answers_photos ap
+  ON (ap.answer_id = an.id)
+  WHERE q.product_id = ${req.query.product_id};`)
+  .then(data => {
+    res.send(data.rows);
+  })
+  .catch(err => {throw err});
+});
 
 
 
